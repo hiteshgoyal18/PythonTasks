42251ffc94ac722a6e43133bbe8464d994979404
hiteshgoyal18 <hitesh.goyal@nlpcaptcha.com>
2018-01-17 09:28:45 +0530

    Update RandomNumberGenerator.py

--- RandomNumberGenerator.py
+++ RandomNumberGenerator.py
@@ -2,14 +2,14 @@ from time import time
 import time as t
  
 def get_seeds():
-    value_1 = value_2 = 0
-    while not value_1 and not value_2:
-#         print int(str(time() - int(time()))[-1]), int(str(time() - int(time()))[-2])
+    seed1=0
+    seed2=0
+    while not seed1 and not seed2:
         try:
-            value_1, value_2 = int(str(time() - int(time()))[-1]), int(str(time() - int(time()))[-2])
-        except ValueError:
+            seed1, seed2 = int(str(time() - int(time()))[-1]), int(str(time() - int(time()))[-2])
+        except:
             get_seeds()
-    return value_1, value_2
+    return seed1, seed2
 
     # ensure relationship of seeds fits that of an lcg and return the "random" result
 def validate_inputs(modulus, increment):
@@ -32,17 +32,16 @@ def generate(modulus, increment):
          
     while current_iteration != seed and current_iteration != 0:
         if not switch:
-            current_iteration = (seed * multiplier) + increment
+            current_iteration = ((seed * multiplier) + increment) % modulus
             switch = True
         else:
-            current_iteration = (current_iteration * multiplier) + increment
+            current_iteration = ((current_iteration * multiplier) + increment) % modulus
             if current_iteration > modulus:
                 current_iteration %= modulus
             output.append(current_iteration)
 
     result = int(str(''.join([str(i) for i in output]))[-1])
     return result
-start=t.ctime().split(" ")[3]
 maxlist=[]
 minlist=[]
 m=10
@@ -50,9 +49,7 @@ i=0
 loop=True
 times=100
 while loop:
-#     t.sleep(0.00000000000001)
     num=generate(m,i)
-    #print num
     if num >= 5:
         if len(maxlist)<73:
             maxlist.append(num)
@@ -61,8 +58,5 @@ while loop:
             minlist.append(num)
     if len(maxlist)==73 and len(minlist)==27:
         loop=False
-# end=t.ctime().split(" ")[3]
-
-print(len(minlist),len(maxlist))
 print(minlist)
 print(maxlist)
